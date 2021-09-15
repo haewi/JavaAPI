1965d671ea3900623d8818a931a0f14d255f92fe
Yeahn <21900151@handong.edu>
2021-09-15 21:42:03 +0900

    naming convention

--- javaAPI/src/javaAPI/connect6API.java
+++ javaAPI/src/javaAPI/connect6API.java
@@ -4,19 +4,19 @@ public class connect6API implements inter {
 
 	@Override
 	public int connect(String ip, int port, String color, String red_stones) {
-		// TODO connect with server
+		// TODO connect to server
 		return 0;
 	}
 
 	@Override
-	public int draw_and_wait(String home, String away) {
-		// TODO give server the home value and get away which will be returned
+	public int drawAndWait(String home, String away) {
+		// TODO give home and put the string from server to away (how to do this without pointer?)
 		return 0;
 	}
 
 	@Override
-	public char get_board(String ask) {
-		// TODO Auto-generated method stub
+	public char getBoard(String ask) {
+		// TODO give the state of the position asked
 		return 0;
 	}
 
--- javaAPI/src/javaAPI/inter.java
+++ javaAPI/src/javaAPI/inter.java
@@ -2,6 +2,6 @@ package javaAPI;
 
 public interface inter {
 	int connect(String ip, int port, String color, String red_stones) ;
-	int draw_and_wait(String home, String away) ;
-	char get_board(String ask);
+	int drawAndWait(String home, String away) ;
+	char getBoard(String ask);
 }
